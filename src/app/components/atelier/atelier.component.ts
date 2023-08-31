9adcc6b30952f35cb9c56012b7ea8336629ab2f2
Alexandre GBAMELE <alexdelor2022@gmail.com>
2023-08-31 12:04:15 +0000

    le service des carousel

--- src/app/components/atelier/atelier.component.ts
+++ src/app/components/atelier/atelier.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Produit } from 'src/app/shared/data/produit';
+import { ProduitService } from 'src/app/shared/services/produit.service';
 
 @Component({
   selector: 'app-atelier',
   templateUrl: './atelier.component.html',
   styleUrls: ['./atelier.component.css']
 })
-export class AtelierComponent {
+export class AtelierComponent implements OnInit {
+  products: Produit[] | undefined;
 
+  responsiveOptions: any[] | undefined;
+
+  constructor(private productService: ProduitService) { }
+
+  ngOnInit() {
+    this.productService.getProductsSmall().then((products) => {
+      this.products = products;
+    });
+
+    this.responsiveOptions = [
+      {
+        breakpoint: '1199px',
+        numVisible: 1,
+        numScroll: 1
+      },
+      {
+        breakpoint: '991px',
+        numVisible: 2,
+        numScroll: 1
+      },
+      {
+        breakpoint: '767px',
+        numVisible: 1,
+        numScroll: 1
+      }
+    ];
+  }
+
+  getSeverity(status: string) {
+    switch (status) {
+      case 'INSTOCK':
+        return 'success';
+      case 'LOWSTOCK':
+        return 'warning';
+      case 'OUTOFSTOCK':
+        return 'danger';
+    }
+  }
 }
+
